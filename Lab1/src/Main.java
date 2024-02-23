b82bfc53d81ee5678271c1f48d0b7b0827916dde
cipri7 <102433321+cipri7@users.noreply.github.com>
2024-02-23 12:29:58 +0200

    First commit

---
+++ Lab1/src/Main.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Dati un numar de la tastatura: ");
+        int a = scanner.nextInt();
+        System.out.println("Numarul este " +a);
+    }
+
+}
